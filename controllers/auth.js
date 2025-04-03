fe7e6be6c018f3ae654b2c2aadf8ec5510df55b3
Fuad2199 <fuad.beybutov099@gmail.com>
2025-04-03 17:51:56 +0400

    error added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,12 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
+import BadRequestError from "../errors/bad-request.js"
 
 export const register = async (req, res) => {
+    const {name, email, password} = req.body
+    if (!name || !email || !password) {
+        throw new BadRequestError('Please provide name, email and password')
+    }
     const user = await User.create({...req.body})
     res.status(StatusCodes.CREATED).json({ user })
 }
